00db5a7d9cd41a321846ec2734df5b4b660c4c8c
Hamdy01221@@ <hamdeahmed01221@gmail.com>
2024-08-01 17:20:46 +0300

    Ecommerce Admin Panel Setup And Mastering

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,3 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 /** Admin Route */
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+/** Profile Routes */
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
+Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
