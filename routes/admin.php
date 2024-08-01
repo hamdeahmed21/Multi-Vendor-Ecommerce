26fed19eea220b2b22a4d1bd84b0d38e6b5a802f
Hamdy01221@@ <hamdeahmed01221@gmail.com>
2024-08-01 15:43:02 +0300

    Ecommerce Multi Auth Setup With Breeze

---
+++ routes/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+
+use App\Http\Controllers\Backend\AdminController;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+
+/** Admin Route */
+Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
---
+++ routes/vendor.php
@@ -0,0 +1,11 @@
+<?php
+
+
+use App\Http\Controllers\Backend\VendorController;
+use Illuminate\Support\Facades\Route;
+
+
+
+/**  Vendor Route*/
+Route::get('dashboard', [VendorController::class, 'dashboard'])->name('dashboard');
+
